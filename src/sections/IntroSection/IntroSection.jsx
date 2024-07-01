d4aff7672a6ddf390f0d499a5e33825cecca839e
Firdovsirz <146226087+Firdovsirz@users.noreply.github.com>
2024-07-02 01:07:46 +0400

    first commit

---
+++ src/sections/IntroSection/IntroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import JobImage from "/src/assets/intro-info-head.avif"
+
+export default function IntroSection() {
+    return (
+        <section className='intro-section'>
+            <div className='intro-info'>
+                <div className='job-title'>
+                    <img src={} alt="" />
+                </div>
+            </div>
+            <div className='intro-profile-img'>
+                <img src="" alt="" />
+            </div>
+        </section>
+    )
+}
